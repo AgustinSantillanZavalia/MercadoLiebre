d9d00be1ff86207906d5d4d191ab462cfc96037b
AgustinSantillanZavalia <148170990+AgustinSantillanZavalia@users.noreply.github.com>
2023-11-09 13:38:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('public'));
+
+/*
+app.listen(3030, () => console.log('servidor corriendo.'))
+
+*/
+//app.use(express.static('public'));
+
+const port = process.env.PORT || 3001;
+
+app.listen (port, () => console.log(`Servidor funcionando bien en puerto ${port}`));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+})
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
